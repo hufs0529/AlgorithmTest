ab6187e452c3d8bfaeb747467638d516f6fb538b
JeongHun Kim <hufs0529@naver.com>
2022-10-11 23:40:37 +0900

    commit

---
+++ 연습.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+t=  int(input())
+for _ in range(t):
+  n, m = map(int, input().split())
+  s = list(map(int, input().split()))
+  cnt = 0
+  queue = deque([s])
+  while queue:
+    best = max(queue)
+    front = queue.popleft()
+    m -= 1
+    
+    if best == front:
+      cnt += 1
+      if m < 0:
+        print(cnt)
+        break
+    else:
+      queue.append(front)
+      if m < 0:
+        m = len(queue) - 1
