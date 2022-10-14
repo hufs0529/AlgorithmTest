52252bdb0a57a785ce0fd80ee21f94c2bfb1cc5d
JeongHun Kim <hufs0529@naver.com>
2022-10-14 21:54:16 +0900

    commit

--- 그리디/백준/그리디_1049.py
+++ 그리디/백준/그리디_1049.py
@@ -10,13 +10,14 @@ for _ in range(m):
   one_list.append(b)
 six_list.sort()
 one_list.sort()
+six = six_list[0]
+one = one_list[0]
   
-  
-if six_list[0] <= one_list[0] * 6:
-  money = six_list[0] * (n // 6) + one_list[0] * (n%6)
-  if six_list[0] < one_list[0] * (n % 6):
-    money = six_list[0] * (n//6 + 1)
+if six <= one * 6:
+  money = six * (n // 6) + one * (n % 6)
+  if six < one * (n % 6):
+    money = six * (n//6 + 1)
 else:
-  money = one_list[0] * n
+  money = one * n
   
 print(money)
