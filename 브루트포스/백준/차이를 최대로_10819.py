106e1ac321c12365283e4b59ecba12ebd7e8d685
JeongHun Kim <hufs0529@naver.com>
2022-10-21 22:53:04 +0900

    2022_10_21

---
+++ 브루트포스/백준/차이를 최대로_10819.py
@@ -0,0 +1,16 @@
+from itertools import permutations
+
+n = int(input())
+s = list(map(int, input().split()))
+
+per = permutations(s)
+ans = 0
+
+for i in per:
+  tmp = 0
+  for j in range(len(i) - 1):
+    tmp += abs(i[j] - i[j+1])
+  
+  if tmp > ans:
+    ans = tmp
+print(ans)
