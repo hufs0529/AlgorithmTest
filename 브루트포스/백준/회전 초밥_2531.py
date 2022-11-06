ff952f5671f42001741b366f900427f654cdc977
JeongHun Kim <hufs0529@naver.com>
2022-11-06 23:27:59 +0900

    2022_11_06

--- 브루트포스/백준/회전 초밥_2531.py
+++ 브루트포스/백준/회전 초밥_2531.py
@@ -2,7 +2,7 @@ n, d, k, c = map(int, input().split())
 s = []
 for _ in range(n):
   s.append(int(input()))
-answer = 0
+answer = 0, 0
 lp, rp = 0, 0
 
 while lp != n:
@@ -14,11 +14,11 @@ while lp != n:
     case.add(s[i])
     if s[i] == c:
       addable = False
-      
+  
   cnt = len(case)
   if addable:
     cnt += 1
   answer = max(answer, cnt)
   lp += 1
-  
+
 print(answer)
