12fbb600e62cb31f28c9bb401fc8cc618c0ae6d6
Github <hufs0529@naver.com>
2022-12-01 21:28:26 +0900

    2022_12_01

--- 다이나믹/백준/오르막 수_11057.py
+++ 다이나믹/백준/오르막 수_11057.py
@@ -3,9 +3,12 @@ dp = [[0] * 10 for _ in range(1001)]
 
 for i in range(10):
   dp[1][i] = 1
-for i in range(2, 1001):
+  
+for i in range(2, n+1):
   for j in range(10):
-    for k in range(j+1):
-      dp[i][j] += dp[i-1][k]
+    if j == 0:
+      dp[i][j] = 1
+    else:
+      dp[i][j] = dp[i-1][j] + dp[i][j-1]
       
 print(sum(dp[n]) % 10007)
