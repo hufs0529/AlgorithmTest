54d55ba932ac7a0f3fc495388635b5d5c4376154
Github <hufs0529@naver.com>
2022-12-18 23:18:42 +0900

    2022_12_18

--- 백트래킹/백준/도영이가 만든 음식_2961.py
+++ 백트래킹/백준/도영이가 만든 음식_2961.py
@@ -6,22 +6,25 @@ def dfs(depth, start):
         for i in arr:
             sour *= i[0]
             bitter += i[1]
-        if abs(sour - bitter) < result:
+        if abs(sour - bitter) <result:
             result = abs(sour - bitter)
         return
+    
     for i in range(start, n):
         arr.append(s[i])
         dfs(depth+1, i+1)
         arr.pop()
-        
-        
+
+
 n = int(input())
 s = []
-arr = []
-result = 999
 for _ in range(n):
     s.append(list(map(int, input().split())))
-    
+arr = []
+result = 99999
+  
 for i in range(1, n+1):
     len_ = i
     dfs(0, 0)
+    
+print(result)
