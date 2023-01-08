527258cd92bbbb5bd3ee5dad4b98978cc054fe2c
Github <hufs0529@naver.com>
2023-01-08 12:03:31 +0900

    2023_01_08

--- 자료구조/괄호의값_2504.py
+++ 자료구조/괄호의값_2504.py
@@ -1,29 +1,32 @@
 s = input()
+stack = []
 ans = 0
 tmp = 1
-stack = []
-
 for i in range(len(s)):
     if s[i] == '(':
         stack.append(s[i])
-        tmp*=2
+        tmp *= 2
+    
     elif s[i] == '[':
         stack.append(s[i])
-        tmp*=3
+        tmp *= 3
+    
     elif s[i] == ')':
-        if not stack or s[-1] == '[':
+        if not stack or stack[-1] == '[':
             ans = 0
             break
         if s[i-1] == '(':
             ans += tmp
         stack.pop()
         tmp //= 2
+        
     else:
-        if not stack or s[-1] == '(':
+        if not stack or stack[-1] == '(':
             ans = 0
             break
         if s[i-1] == '[':
             ans += tmp
+        
         stack.pop()
         tmp //= 3
         
--- 자료구조/키로거_5397.py
+++ 자료구조/키로거_5397.py
@@ -1,18 +1,18 @@
 n = int(input())
 for _ in range(n):
-    s = input()
-    left, right = [], []
-    for i in s:
-        if i == '<':
-            if left:
-                right.append(left.pop())
-        elif i == '>':
-            if right:
-                left.append(right.pop())
-        elif i == '-':
-            if left:
-                left.pop()
-        else:
-            left.append(i)
-    left.extend(reversed(right))
-    print(''.join(left))
+  s = input()
+  left, right = [], []
+  for i in s:
+    if  i == '<':
+      if left:
+        right.append(left.pop())
+    elif i == '>':
+      if right:
+        left.append(right.pop())
+    elif i == '-':
+      if left:
+        left.pop()
+    else:
+      left.append(i)
+  left.extend(reversed(right))
+  print(''.join(left))
