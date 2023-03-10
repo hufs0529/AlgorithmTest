6dc50e9496c80f0e2388d0196e5cc11e62ef7b8e
Github <hufs0529@naver.com>
2023-03-10 23:58:21 +0900

    2023_03_10

--- 연습.py
+++ 연습.py
@@ -1,12 +1,14 @@
 n = int(input())
-k = int(input())
-s = list(map(int, input().split()))
-s.sort()
+dp = [0] * (n+1)
+for i in range(n+1):
+    dp[i] = [0, 0, 0]
+dp[1][0] = 1
+dp[1][1] = 1
+dp[1][2] = 1
 
-arr = []
-for i in range(n-1):
-    arr.append(s[i+1] - s[i])
-    
-arr.sort()
+for i in range(2, n+1):
+    dp[i][0] = dp[i-1][0] + dp[i-1][1] + dp[i-1][2]
+    dp[i][1] = dp[i-1][0] + dp[i-1][2]
+    dp[i][2] = dp[i-1][0] + dp[i-1][1]
 
-print(sum(arr[:n-k]))
+print(sum(dp[n]) % 9901)
