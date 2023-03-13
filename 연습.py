93521f59caf02a2dcbe23f4c4dc730f0d6699854
Github <hufs0529@naver.com>
2023-03-13 10:23:30 +0900

    2023_03_13

--- 연습.py
+++ 연습.py
@@ -1,14 +1,14 @@
-n = int(input())
-dp = [0] * (n+1)
-for i in range(n+1):
-    dp[i] = [0, 0, 0]
-dp[1][0] = 1
-dp[1][1] = 1
-dp[1][2] = 1
+n, m = map(int, input().split())
+s = []
+for _ in range(n):
+    s.append(list(map(int, input().split())))
+dp = [[0] * (n+1) for _ in range(n+1)]
 
-for i in range(2, n+1):
-    dp[i][0] = dp[i-1][0] + dp[i-1][1] + dp[i-1][2]
-    dp[i][1] = dp[i-1][0] + dp[i-1][2]
-    dp[i][2] = dp[i-1][0] + dp[i-1][1]
-
-print(sum(dp[n]) % 9901)
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        dp[i][j] = dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1] + s[i-1][j-1]
+        
+for _ in range(m):
+    x1, y1, x2, y2 = map(int, input().split())
+    
+    print(dp[x2][y2] + dp[x1-1][y1-1] - dp[x1-1][y2] - dp[x2][y1-1])
