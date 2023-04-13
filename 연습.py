bb309b5e1d038565b7ee9c6d5590fc268035e1db
Github <hufs0529@naver.com>
2023-04-13 21:38:48 +0900

    2023_04_13

--- 연습.py
+++ 연습.py
@@ -3,8 +3,7 @@ import pandas_datareader as pdr
 from datetime import datetime 
  
 start_date = datetime(2021,1,1) 
-end_date = datetime(2021,1,1)
+end_date = datetime(2021,1,5)
  
-df = pdr.DataReader('AAPL', 'yahoo', start_date, end_date) 
-## 또는 pdr.get_data_yahoo('AAPL', start_date, end_date)
-df.to_csv()
+df = pdr.DataReader('AAPL', 'yahoo', start_date, end_date)
+print(df)
