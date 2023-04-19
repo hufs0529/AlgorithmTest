ce56978a79ef1351bf4d2f52f1361ba40728cffe
Github <hufs0529@naver.com>
2023-04-19 23:08:12 +0900

    2023_04_19

--- 연습.py
+++ 연습.py
@@ -1,9 +1,11 @@
 import pandas_datareader as pdr
+import datetime
 
-from datetime import datetime 
- 
-start_date = datetime(2021,1,1) 
-end_date = datetime(2021,1,5)
- 
-df = pdr.DataReader('AAPL', 'yahoo', start_date, end_date)
-print(df)
+start_date = datetime.datetime(2010, 1, 1)
+end_date = datetime.datetime(2021, 12, 31)
+
+# Use DataReader function from pandas_datareader to download the historical stock prices
+aapl_stock_prices = pdr.DataReader('AAPL', data_source='yahoo', start=start_date, end=end_date)
+
+# Print the first 5 rows of the dataframe
+print(aapl_stock_prices.head())
