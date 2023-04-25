a3782c6f4a6e963cb8b8d68b81c603bafc618e4a
Github <hufs0529@naver.com>
2023-04-25 10:16:07 +0900

    2023_04_25

--- 연습.py
+++ 연습.py
@@ -1,11 +1,7 @@
-import pandas_datareader as pdr
-import datetime
+from pykiwoom.kiwoom import *
 
-start_date = datetime.datetime(2010, 1, 1)
-end_date = datetime.datetime(2021, 12, 31)
+kiwoom = Kiwoom()
+kiwoom.CommConnect(block=True)
 
-# Use DataReader function from pandas_datareader to download the historical stock prices
-aapl_stock_prices = pdr.DataReader('AAPL', data_source='yahoo', start=start_date, end=end_date)
-
-# Print the first 5 rows of the dataframe
-print(aapl_stock_prices.head())
+name = kiwoom.GetMasterCodeName("005930")
+print(name)
