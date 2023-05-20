330789f5f6f762d99fd9f341b9015f67dbe6c671
Github <hufs0529@naver.com>
2023-05-20 21:50:46 +0900

    2023_05_20

--- 연습.py
+++ 연습.py
@@ -1,20 +1,8 @@
-N, L = map(int, input().split())
-pool = [list(map(int, input().split())) for _ in range(N)]
-pool.sort(key=lambda x: x[0])
-plank = pool[0][0]
-total = 0
+s = input()
 
-for start, end in pool:
-  if plank > end:
-    continue
-  elif plank < start:
-    plank = start
-    
-  dist = end - plank
-  remainder = 1
-  if dist % L == 0:
-    remainder = 0
-  count = dist // L + remainder
-  plank += count * L
-  total += count
-print(total)
+cnt = 0
+for i in range(len(s)-1):
+  if s[i] != s[i+1]:
+    cnt += 1
+
+print((cnt+1)//2)
