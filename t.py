48c4b8f38d84a3ebdc86b03da86ece0a065c248e
JeongHun Kim <81501114+hufs0529@users.noreply.github.com>
2023-07-13 00:42:47 +0000

    2023_07_13

--- t.py
+++ t.py
@@ -1,16 +1,35 @@
-n = int(input())
-tree = list(map(int, input().split()))
-erase = int(input())
+from collections import deque
 
-def dfs(v):
-	tree[v] = -2
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+n, m = map(int, input().split())
+graph = []
+for _ in range(n):
+	graph.append(list(map(int, input().split())))
+
+def bfs(a, b):
 	for i in range(n):
-		if v == tree[i]:
-			dfs(i)
-dfs(erase)
-cnt = 0
+		for j in range(m):
+			if graph[i][j] == 2:
+				queue.append((i, j))
+	
+	while queue:
+		x, y = queue.popleft()
+		for i in range(4):
+			nx = x + dx[i]
+			ny = y + dy[i]
+			if 0 <= nx < n and 0 <= ny < m and graph[nx][n] == 0:
+				graph[nx][ny] = 2
+				queue.append([nx, ny])
+	
+	global result
+	cnt = 0
+	for i in range(n):
+		for j in range(m):
+			if graph[i][j] == 0:
+				cnt += 1
+	result = max(result, cnt)
 
-for i in range(n):
-	if tree[i] == -2 and i not in tree:
-		cnt += 1
 
+result = 0
