558826f6df3f09b56e1ffc09ffb015e59d8bc5f4
JeongHun Kim <81501114+hufs0529@users.noreply.github.com>
2023-07-19 00:51:54 +0000

    2023_07_19

--- t.py
+++ t.py
@@ -1,24 +1,24 @@
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+from bisect import bisect_left
 
-r, c = map(int, input().split())
-board = []
-for _ in range(r):
-	board.append(list(input()))
-ans = 0
-alpha = set()
+n, h = map(int, input().split())
+down, up = [], []
 
-def dfs(x, y, count):
-	global ans
-	ans = max(ans, count)
-	for i in range(4):
-		nx = x + dx[i]
-		ny = y + dy[i]
-		if 0 <= nx < r and 0 <= ny < c and not board[nx][ny] in alpha:
-			alpha.add(board[nx][ny])
-			dfs(nx, ny, count+1)
-			alpha.remove(board[nx][ny])
+for i in range(n):
+	if i%2 == 0:
+		down.append([int(input())])
+	else:
+		up.append([int(input())])
 
-alpha.add(board[0][0])
-dfs(0,0,1)
-print(ans)
+down.sort()
+up.sort()
+cnt = 1
+
+min_val = 1e9
+for i in range(1, h+1):
+	t, b = bisect_left(up, (h+1)-i), bisect_left(down, i)
+	total = n - (t+b)
+	if total < min_val:
+		min_val = total
+		cnt = 1
+	elif total == min_val:
+		cnt += 1
