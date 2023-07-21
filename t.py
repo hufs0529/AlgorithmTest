f3d6df7260087ee5608913da702dbfafbee622be
JeongHun Kim <81501114+hufs0529@users.noreply.github.com>
2023-07-21 00:28:24 +0000

    2023_07_21

--- t.py
+++ t.py
@@ -1,18 +1,11 @@
 n, k = map(int, input().split())
-stuff = [[0, 0]]
-knapsack = [[0 for _ in range(k+1)] for _ in range(n+1)]
+coin = [int(input()) for _ in range(n)]
+dp = [0 for i in range(k+1)]
+dp[0] = 1
 
-for _ in range(n):
-	stuff.append(list(map(int, input().split())))
+for i in c:
+	for j in range(i, k+1):
+		if j -i > 0:
+			dp[j] += dp[j-i]
 
-for i in range(1, n+1):
-	for j in range(1, k+1):
-		weight = stuff[i][0] 
-		value = stuff[i][1]
-
-		if j < weight:
-			knapsack[i][j] = knapsack[i-1][j]
-		else:
-			knapsack[i][j] = max(value + knapsack[i-1][j-weight], knapsack[i-1][j])
-
-print(knapsack[n][k])
+print(dp[k])
