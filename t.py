efd7dd329c2c9ff9110e7fa7c0931b29cb934330
JeongHun Kim <81501114+hufs0529@users.noreply.github.com>
2023-07-27 00:51:16 +0000

    2023_07_27

--- t.py
+++ t.py
@@ -1,14 +1,16 @@
-n, m = map(int, input().split())
+r, c, q = map(int, input().split())
 s = []
-dp = [[0] * (n+1) for i in range(n+1)]
-for _ in range(n):
+dp = [[0 for _ in range(c+1)] for _ in range(r+1)]
+
+for _ in range(r):
     s.append(list(map(int, input().split())))
 
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        dp[i][j] = dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1] + s[i-1][j-1]
+for i in range(1, r+1):
+    for j in range(1, c+1):
+        dp[i][j] = s[i-1][j-1] - dp[i-1][j-1] + dp[i][j-1] + dp[i-1][j]
 
-for _ in range(m):
+for _ in range(q):
     x1, y1, x2, y2 = map(int, input().split())
-    result = dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1]
-    print(result)
+    ans = dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1]
+    num = ((x2-x1)+1) * ((y2-y1)+1)
+    print(ans // num)
