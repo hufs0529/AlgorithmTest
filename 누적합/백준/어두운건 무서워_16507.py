03ba85401c8246c54c9b749cbcaeabe42fd51732
JeongHun Kim <81501114+hufs0529@users.noreply.github.com>
2023-08-04 00:57:15 +0000

    2023_08_04

---
+++ 누적합/백준/어두운건 무서워_16507.py
@@ -0,0 +1,15 @@
+r, c, q = map(int, input().split())
+s = []
+for _ in range(r):
+    s.append(list(map(int, input().split())))
+dp = [[0 for _ in range(c+1)] for _ in range(r+1)]
+
+for i in range(1, r+1):
+    for j in range(1, c+1):
+        dp[i][j] = s[i-1][j-1] - dp[i-1][j-1] + dp[i-1][j] + dp[i][j-1]
+
+for _ in range(q):
+    x1, y1, x2, y2 = map(int, input().split())
+    ans = dp[x2][y2] + dp[x1-1][y1-1] - dp[x2][y1-1] - dp[x1-1][y2]
+    num = ((x2-x1)+1) * ((y2-y1)+1)
+    print(ans//num)
--- 누적합/백준/체스판다시칠하기2_25682.py
+++ 누적합/백준/체스판다시칠하기2_25682.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+sys.maxsize
 n, m, k = map(int, input().split())
 s = []
 for _ in range(n):
@@ -5,8 +8,8 @@ for _ in range(n):
 row = n-k+1
 column = m-k+1
 
-def sol(color):
-    dp = [[0]*(m+1) for _ in range(n+1)]
+def make_chess(color):
+    dp = [[0] * (m+1) for _ in range(n+1)]
     for i in range(n):
         for j in range(m):
             if (i+j)%2 == 0:
@@ -14,12 +17,11 @@ def sol(color):
             else:
                 value = s[i][j] == color
             dp[i+1][j+1] = dp[i][j+1] + dp[i+1][j] - dp[i][j] + value
-        count = sys.maxsize
+    count = sys.maxsize
 
-        for i in range(1, row+1):
-            for j in range(1, column+1):
-                count = min(count, dp[i+k-1][j+k-1]-dp[i+k-1][j-1]-dp[i-1][j+k-1]+dp[i-1][j-1])
-            
-        return count
+    for i in range(1, row+1):
+        for j in range(1, column+1):
+            count = min(count, dp[i+k-1][j+k-1] - dp[i+k-1][j-1] - dp[i-1][j+k-1] + dp[i-1][j-1])
+    return count
 
-    
+print(min(make_chess('B'), make_chess('W')))
