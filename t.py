1057874ac45b65d9d572fd776d9765bf508e46d9
JeongHun Kim <hufs0529@naver.com>
2023-08-15 14:55:29 +0900

    2023_08_15

--- t.py
+++ t.py
@@ -1,24 +1,14 @@
-n, m, k = map(int, input().split())
-s = []
-for _ in range(n):
-    s.append(list(input()))
-row = n-k+1
-column = m-k+1
+import sys
 
-def sol(color):
-    dp = [[0]*(m+1) for _ in range(n+1)]
-    for i in range(n):
-        for j in range(m):
-            if (i+j)%2 == 0:
-                value = s[i][j] != color
-            else:
-                value = s[i][j] == color
-            dp[i+1][j+1] = dp[i][j+1] + dp[i+1][j] - dp[i][j] + value
-        count = sys.maxsize
+N, L, W, H = map(int, sys.stdin.readline().split())
+S, E = 0, max(L, W, H)
 
-        for i in range(1, row+1):
-            for j in range(1, column+1):
-                count = min(count, dp[i+k-1][j+k-1]-dp[i+k-1][j-1]-dp[i-1][j+k-1]+dp[i-1][j-1])
-            
-        return count
-    
+for _ in range(10000):
+    M = (S + E) / 2
+    count = (L // M) * (W // M) * (H // M)
+    if count >= N:
+        S = M
+    else:
+        E = M
+
+print("%.10f" %(E))
