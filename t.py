12109060445854b157a4c4c3cb05198a942df22f
JeongHun Kim <81501114+hufs0529@users.noreply.github.com>
2023-08-18 00:42:43 +0000

    2023_08_18

--- t.py
+++ t.py
@@ -1,24 +1,18 @@
-n, m, k = map(int, input().split())
-s = []
-for _ in range(n):
-    s.append(list(input()))
-row = n-k+1
-column = m-k+1
+n = int(input())
 
-def sol(color):
-    dp = [[0]*(m+1) for _ in range(n+1)]
-    for i in range(n):
-        for j in range(m):
-            if (i+j)%2 == 0:
-                value = s[i][j] != color
-            else:
-                value = s[i][j] == color
-            dp[i+1][j+1] = dp[i][j+1] + dp[i+1][j] - dp[i][j] + value
-        count = sys.maxsize
+dp = [[0]*10 for _ in range(n+1)]
+for i in range(1, 10):
+    dp[1][i] = 1
 
-        for i in range(1, row+1):
-            for j in range(1, column+1):
-                count = min(count, dp[i+k-1][j+k-1]-dp[i+k-1][j-1]-dp[i-1][j+k-1]+dp[i-1][j-1])
-            
-        return count
-    
+MOD = 1000000000
+
+for i in range(2, n+1):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = dp[i-1][1]
+        elif j == 9:
+            dp[i][j] = dp[i-1][8]
+        else:
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
+
+print(dp)
