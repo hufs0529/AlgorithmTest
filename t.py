aeeaa864bd9314dff1e672745cfcab4817540823
JeongHun Kim <81501114+hufs0529@users.noreply.github.com>
2023-09-03 08:01:37 +0000

    2023_09_03

--- t.py
+++ t.py
@@ -1,17 +1,13 @@
-n, m = map(int, input().split())
-s = []
-for _ in range(n):
-    s.append(int(input()))
-s.sort()
+n = int(input())
+dp = [[0] * 10 for _ in range(n+1)]
 
-left, right = 0, 0
-result = int(2e9)
+for i in range(10):
+    dp[1][i] = 1
+for i in range(2, n+1):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = 1
+        else:
+            dp[i][j] = dp[i-1][j] + dp[i][j-1]
 
-while left <= right and right < n:
-    if s[right]-s[left]< m:
-        right += 1
-    else:
-        result = min(result, s[right]-s[left])
-        left += 1
-
-print(result)
+print(sum(dp[n]) % 10007)
