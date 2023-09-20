0ef7cec7b0e8ab976b7a1b076954dbbe20201434
JeongHun Kim <81501114+hufs0529@users.noreply.github.com>
2023-09-20 09:33:07 +0000

    2023_09_20

--- t.py
+++ t.py
@@ -7,23 +7,25 @@ for _ in range(k):
     for i in range(y1, y2):
         for j in range(x1, x2):
             graph[i][j] = 1
-cnt = 0
+cnt = 1
 
 def dfs(x, y):
     global cnt
-    cnt += 1
     graph[x][y] = 1
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
         if 0 <= nx < m and 0 <= ny < n and graph[nx][ny] == 1:
+            cnt += 1
             dfs(nx, ny)
-    return cnt
 
+area = 0
 result = []
 for i in range(m):
     for j in range(n):
-        cnt = dfs(i, j)
-        if cnt:
+        if graph[i][j] == 0:
+            area += 1
+            dfs(i, j)
             result.append(cnt)
-            
+            cnt = 1
+
