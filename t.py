a65c908ca1390326851488c2a3f0da5a91ea5941
JeongHun Kim <81501114+hufs0529@users.noreply.github.com>
2023-09-20 09:30:35 +0000

    2023_09_20

--- t.py
+++ t.py
@@ -1,27 +1,29 @@
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
-n, m = map(int, input().split())
-graph = []
-visited = [[False] * m for _ in range(n)]
-for _ in range(n):
-    graph.append(list(map(int, input().split())))
+m, n, k = map(int, input().split())
+graph = [[0] * n for _ in range(m)]
+for _ in range(k):
+    x1, y1, x2, y2 = map(int, input().split())
+    for i in range(y1, y2):
+        for j in range(x1, x2):
+            graph[i][j] = 1
 cnt = 0
 
 def dfs(x, y):
     global cnt
-    if x == (n-1) and y == (m-1):
-        print(cnt)
-        return
-
-    visited[x][y] = True
+    cnt += 1
+    graph[x][y] = 1
     for i in range(4):
         nx = x + dx[i]
-        ny = y + dy[i]  
+        ny = y + dy[i]
+        if 0 <= nx < m and 0 <= ny < n and graph[nx][ny] == 1:
+            dfs(nx, ny)
+    return cnt
 
-        if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == False and graph[nx][ny] == 1:
-            visited[nx][ny] = True
-            cnt += 1
-
+result = []
+for i in range(m):
+    for j in range(n):
+        cnt = dfs(i, j)
+        if cnt:
+            result.append(cnt)
             
-
-dfs(0, 0)
