fe888543de7adb6bce6c986df87b721904ef0ba9
JeongHun Kim <81501114+hufs0529@users.noreply.github.com>
2023-09-21 06:17:54 +0000

    2023_09_21

--- t.py
+++ t.py
@@ -1,31 +1,33 @@
+def bfs(a, b):
+    queue = deque()
+    queue.append((a, b))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < m and 0 <= ny < n:
+                if graph[nx][ny] == 1:
+                    queue.append((nx, ny))
+                    graph[nx][ny] = 0
+
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
-m, n, k = map(int, input().split())
-graph = [[0] * n for _ in range(m)]
-for _ in range(k):
-    x1, y1, x2, y2 = map(int, input().split())
-    for i in range(y1, y2):
-        for j in range(x1, x2):
-            graph[i][j] = 1
-cnt = 1
 
-def dfs(x, y):
-    global cnt
-    graph[x][y] = 1
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < m and 0 <= ny < n and graph[nx][ny] == 1:
-            cnt += 1
-            dfs(nx, ny)
+from collections import deque
 
-area = 0
-result = []
-for i in range(m):
-    for j in range(n):
-        if graph[i][j] == 0:
-            area += 1
-            dfs(i, j)
-            result.append(cnt)
-            cnt = 1
-
+t = int(input())
+for _ in range(t):
+    cnt = 0
+    m, n, k = map(int, input().split())
+    graph = [[0] * n for _ in range(m)]
+    for _ in range(k):
+        x, y = map(int, input().split())
+        graph[x][y] = 1
+    
+    for a in range(m):
+        for b in range(n):
+            if graph[a][b] == 1:
+                bfs(a, b)
+                cnt += 1
+    print(cnt)
