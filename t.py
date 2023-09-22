54c38e82fba99ff38892cfcd7db5c4cd20dc94ff
JeongHun Kim <81501114+hufs0529@users.noreply.github.com>
2023-09-22 09:54:55 +0000

    2023_09_22

--- t.py
+++ t.py
@@ -1,33 +1,17 @@
-def bfs(a, b):
-    queue = deque()
-    queue.append((a, b))
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < m and 0 <= ny < n:
-                if graph[nx][ny] == 1:
-                    queue.append((nx, ny))
-                    graph[nx][ny] = 0
+def sol(s):
+    for i in range(n-s+1):
+        for j in range(m-s+1):
+            if arr[i][j] == arr[i][j+s-1] == arr[i+s-1][j] == arr[i+s-1][j+s-1]:
+                return True
+    return False
 
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+n, m = map(int, input().split())
+arr = [list(map(int, list(input()))) for _ in range(n)]
 
-from collections import deque
 
-t = int(input())
-for _ in range(t):
-    cnt = 0
-    m, n, k = map(int, input().split())
-    graph = [[0] * n for _ in range(m)]
-    for _ in range(k):
-        x, y = map(int, input().split())
-        graph[x][y] = 1
-    
-    for a in range(m):
-        for b in range(n):
-            if graph[a][b] == 1:
-                bfs(a, b)
-                cnt += 1
-    print(cnt)
+size = min(n, m)
+
+for k in range(size, 0, -1):
+    if sol(k):
+        print(k**2)
+        break
