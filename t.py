f778a5d15300af572d2a3aa57606d77080e5659e
JeongHun Kim <81501114+hufs0529@users.noreply.github.com>
2023-09-24 06:31:51 +0000

    2023_09_24

--- t.py
+++ t.py
@@ -1,26 +1,15 @@
-from itertools import combinations
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    dp = []
+    for _ in range(2):
+        dp.append(list(map(int, input().split())))
+    
+    dp[0][1] += dp[1][0]
+    dp[1][1] += dp[0][0]
 
-n, m = map(int, input().split())
-maps = []
-for _ in range(n):
-    maps.append(list(map(int, input().split())))
-result = 1e9
-houses, chickens = [], []
-
-for i in range(n):
-    for j in range(n):
-        if maps[i][j] == 1:
-            houses.append([i, j])
-        elif maps[i][j] == 2:
-            chickens.append([i, j])
-
-for comb in combinations(chickens, m):
-    temp = 0
-    for h in houses:
-        chicken_len = 999
-        for j in range(m):
-            chicken_len = min(chicken_len, abs(h[0] - comb[j][0]) + abs(h[1] - comb[j][1]))
-        temp += chicken_len
-    result = min(result, temp)
-
-print(result)
+    for i in range(2, n):
+        dp[0][i] += max(dp[1][i-1], dp[1][i-2])
+        dp[1][i] += max(dp[0][i-1], dp[0][i-2])
+    
+    print(max(dp[0][n-1], dp[1][n-1]))
