f2e4f14cb935f59a3ddd8cfc1c1c93fd934d1792
JeongHun Kim <81501114+hufs0529@users.noreply.github.com>
2023-10-01 08:11:41 +0000

    2023_10_01

--- t.py
+++ t.py
@@ -1,13 +1,10 @@
-n,  k = map(int, input().split())
-arr = list(map(int, input().split()))
-dp = [0] * (n+1)
-for i in range(n):
-    dp[i+1] = dp[i] + arr[i]
-cnt = 0
+n, k = map(int, input().split())
+dp = [[0] * (k+1) for _ in range(n+1)]
+dp[0][0] = 1
 
-for i in range(n):
-    for j in range(i+1, n+1):
-        tmp = dp[j] - dp[i]
-        if tmp == k:
-            cnt += 1
+for i in range(0, n+1):
+    for j in range(1, k+1):
+        dp[i][j] = dp[i-1][j] + dp[i][j-1]
 
+print(dp)
+print(dp[n][k] % 1000000000)
