3479aa1cc447586bc02d37a309091e56fb76a2e4
JeongHun Kim <81501114+hufs0529@users.noreply.github.com>
2023-10-09 04:51:36 +0000

    2023_10_09

--- t.py
+++ t.py
@@ -1,23 +1,10 @@
 n = int(input())
-marble = list(map(int, input().split()))
-total = 0
+s = list(map(int, input().split()))
+dp = [1] * n
 
-while n > 2:
-    max_sum = 0
-    max_index = -1
+for i in range(1, n):
+    for j in range(i):
+        if s[j] > s[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
 
-    for i in range(1, n-1):
-        cur = marble[i-1] * marble[i+1]
-        if cur > max_sum:
-            max_sum = cur
-            max_index = i
-
-    total += max_sum
-    del marble[max_index]
-    n -= 1
-
-print(total)
-
-# 1 2 3 4
-# 1 2 4 / n=3, sum = 8
-# 1 4 /n=2, sum = 12
+print(max(dp))
