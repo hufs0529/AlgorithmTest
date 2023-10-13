bf633f18b225a6a61a3d635c61ac81796fc905c4
JeongHun Kim <81501114+hufs0529@users.noreply.github.com>
2023-10-13 08:00:04 +0000

    2023_10_13

--- t.py
+++ t.py
@@ -1,11 +1,16 @@
-n = int(input())
-road = list(map(int, input().split())) # 2 3 1
-cost = list(map(int, input().split())) # 5 2 4 1
+n, m = map(int, input().split())
+line = []
+for _ in range(m):
+    a, b = map(int, input().split())
+    line.append([a, b])
+ans = 0
 
-dp = [0] * n
-dp[0] = cost[0] * road[0]
-m = 0
+st = sorted(line, key=lambda x:x[0])
+each = sorted(line, key=lambda x:x[1])
 
-for i in range(1, n-1):
-    m = min(m, cost[i])
-    dp[i] = m * road[i]
+if st[0][0] <= each[0][1] * 6: # 세트 가격이 낱개 6개보다 저렴할때
+    ans = st[0][0] * (n//6) + each[0][1] * (n%6)
+else: # 세트 가격이 낱개 6개보다 비쌀때
+    ans = each[0][1] * n
+
+print(ans)
