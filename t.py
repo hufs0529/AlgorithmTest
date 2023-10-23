9ff7e521785a3d7f0bfaa1aba037aa6a4b2f9edd
JeongHun Kim <81501114+hufs0529@users.noreply.github.com>
2023-10-23 19:44:54 +0900

    Update t.py

--- t.py
+++ t.py
@@ -1,17 +1,1 @@
-def solve(s, t):
-    while len(t) >= len(s):
-        if t == s:
-            return True
-        if t[-1] == 'A':
-            t = t[:-1]
-        elif t[-1] == 'B':
-            t = t[:-1][::-1]
-    return False
 
-s = input()
-t = input()
-
-if dfs(s, t):
-    print(1)
-else:
-    print(0)
