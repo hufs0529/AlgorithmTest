98692b9d7a19666420023e6ac78f84e227514d71
JeongHun Kim <hufs0529@naver.com>
2023-10-26 16:10:56 +0900

    2023_10_26

--- 구현/백준/222-풀링_17829.py
+++ 구현/백준/222-풀링_17829.py
@@ -1,20 +1,33 @@
-n = int(input())
-graph = []
-for _ in range(n):
-    graph.append(list(map(int, input().split())))
+def find_consecutive_increases(matrix):
+    results = []
 
-def pooling(size, x, y):
-    mid = size // 2
-    if size == 2:
-        answer = [graph[x][y], graph[x+1][y], graph[x][y+1], graph[x+1][y+1]]
-        answer.sort()
-        return answer[-2]
-    lt = pooling(mid, x, y)
-    rt = pooling(mid, x + mid, y)
-    lb = pooling(mid, x, y + mid)
-    rb = pooling(mid, x + mid, y + mid)
-    answer = [lt, rt, lb, rb]
-    answer.sort()
-    return answer[-2]
+    for row in matrix:
+        consecutive_count = 1  # 연속적으로 증가하는 요소의 개수를 추적하기 위한 변수
+        max_consecutive_count = 1  # 최대 연속적으로 증가하는 요소의 개수를 추적하기 위한 변수
 
-print(pooling(n, 0, 0))
+        for i in range(1, len(row)):
+            if row[i] > row[i - 1]:
+                consecutive_count += 1
+            else:
+                consecutive_count = 1
+
+            if consecutive_count > max_consecutive_count:
+                max_consecutive_count = consecutive_count
+
+        results.append(max_consecutive_count)
+
+    return results
+
+# 예시 2차원 배열
+matrix = [
+    [1, 2, 3, 4, 5],
+    [1, 1, 2, 3, 4],
+    [1, 1, 1, 2, 3],
+    [1, 1, 1, 1, 2],
+    [1, 1, 1, 1, 1]
+]
+
+consecutive_increases = find_consecutive_increases(matrix)
+
+for i, count in enumerate(consecutive_increases):
+    print(f"행 {i + 1}: 가장 긴 연속적인 증가 요소 수 = {count}")
