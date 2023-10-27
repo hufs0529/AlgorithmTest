d90a741361cb1a6fc243181792422cbf2c3e0df9
JeongHun Kim <hufs0529@naver.com>
2023-10-27 21:28:15 +0900

    2023_10_27

--- 구현/백준/222-풀링_17829.py
+++ 구현/백준/222-풀링_17829.py
@@ -1,33 +1,22 @@
-def find_consecutive_increases(matrix):
-    results = []
+n, w, l = map(int, input().split())
+trucks = list(map(int, input().split()))
 
-    for row in matrix:
-        consecutive_count = 1  # 연속적으로 증가하는 요소의 개수를 추적하기 위한 변수
-        max_consecutive_count = 1  # 최대 연속적으로 증가하는 요소의 개수를 추적하기 위한 변수
+bridge = [0] * w
+time, weight = 0, 0
 
-        for i in range(1, len(row)):
-            if row[i] > row[i - 1]:
-                consecutive_count += 1
-            else:
-                consecutive_count = 1
+while True:
+    out = bridge.pop(0)
+    weight -= out
 
-            if consecutive_count > max_consecutive_count:
-                max_consecutive_count = consecutive_count
+    if truck:
+        if weight + truck[0] <= l:
+            bridge.append(truck[0])
+            weight += truk[0]
+            truck.append(0)
+        else:
+            bridge.append(0)
+    time += 1
 
-        results.append(max_consecutive_count)
-
-    return results
-
-# 예시 2차원 배열
-matrix = [
-    [1, 2, 3, 4, 5],
-    [1, 1, 2, 3, 4],
-    [1, 1, 1, 2, 3],
-    [1, 1, 1, 1, 2],
-    [1, 1, 1, 1, 1]
-]
-
-consecutive_increases = find_consecutive_increases(matrix)
-
-for i, count in enumerate(consecutive_increases):
-    print(f"행 {i + 1}: 가장 긴 연속적인 증가 요소 수 = {count}")
+    if not bridge:
+        break
+print(time)
